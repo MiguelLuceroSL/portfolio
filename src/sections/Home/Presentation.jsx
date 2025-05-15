0e3c85d2d7b3b0ca393a51fecefd4b2c6113e897
Lucero Roque Roberto Miguel <roquerobertomiguellucero@gmail.com>
2025-05-15 10:07:42 -0300

    add cursor pointer
    agrego cursor pointer al buttom del cv

--- src/sections/Home/Presentation.jsx
+++ src/sections/Home/Presentation.jsx
@@ -17,7 +17,7 @@ const Presentation = () => {
                     <span className="text-[#c4c4c4] text-[1rem] sm:text-[1.5rem] md:text-[2rem] lg:text-[2.5rem]">Soy Desarrollador Web Fullstack.</span>
                     <br />
                     <a className="text-[#00bcd4] text-[0.5rem] sm:text-[1rem] md:text-[1.5rem]" href={CV} target="_blank">
-                        <button className="bg-[#00bcd4] text-white rounded-md px-4 py-2 mt-4 hover:bg-[#008c9e] transition duration-300">
+                        <button className="bg-[#00bcd4] text-white rounded-md px-4 py-2 mt-4 hover:bg-[#008c9e] hover:cursor-pointer transition duration-300">
                             Descargar mi CV    
                         </button>
                     </a>
